cd6e0e832a51c84a6933f53fb66473520d270569
Christian Di Lorenzo <rcdilorenzo@me.com>
2012-08-13 08:47:44 -0400

    Fixed tests to pass

--- features/create_user.feature
+++ features/create_user.feature
@@ -1,24 +1,22 @@
 Feature: Creating a User
+  Background: Create a user
+    Given I create a new user
 
   Scenario: Create existing user
-    When I create a new user
-    Then I sign out
-    And I sign in
+    # Given I see a user page for "John" with statistics
     Then I should be able to start a game
 
   Scenario: Signing out
-    When I create a new user
-    And I sign out
+    When I sign out
     Then I should see a signed out message
 
   Scenario: Signing in
-    When I create a new user
-    Then I sign out
-    And I sign in
+    # I have to sign out because sign in automatically ocurrs after signing up
+    When I sign in after signing out
     Then I should see a signed in message
 
   # Scenario: Create existing user with name
-  #     When I identify an existing user with the first name "John"
+  #     When I finish a game and win
   #     Then I should see the user page for "Christian" with statistics
 
   # Scenario: Create new user with name
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,52 +1,22 @@
-# When /[create|identify] a\w{0,1}.(\w+)\s?user .*"(.*?)"$/ do |type, name|
-#   if type == "existing"
-#     2.times do |count| # if executed twice user will be an existing user
-#       visit('/')
-#       fill_in :first_name, :with => name
-#       fill_in :last_name, :with => "Doe"
-#       fill_in :email, :with => "user@example.com"
-#       fill_in :screen_name, :with => "winnerjohn65"
-#       fill_in :street, :with => "123 Maple Avenue"
-#       fill_in :city, :with => "Raleigh"
-#       fill_in :state, :with => "NC"
-#       fill_in :zip, :with => "98101"
-      
-#       click_button 'Create User'
-#     end
-#   elsif type == "new"
-#     visit('/')
-#     fill_in :first_name, :with => name
-#       fill_in :last_name, :with => "Doe"
-#       fill_in :email, :with => "user@example.com"
-#       fill_in :screen_name, :with => "winnerjohn65"
-#       fill_in :street, :with => "123 Maple Avenue"
-#       fill_in :city, :with => "Raleigh"
-#       fill_in :state, :with => "NC"
-#       fill_in :zip, :with => "98101"
-#       click_button 'Create User'
-#   end
-#   @username = name
-# end
-
 # Example: Then I should see the user page for "Christian" without statistics
 # Example: Then I should see a welcome webpage for "Christian" with statistics
 # Example: Then I should see a user "Christian" with stats
-Then /see.*[welcome|user].*"(.*)".?(\w+).*stat.*s/ do |name, statistics|
-  if statistics == "with"
-    # "Welcome back, Christian"
-    page.body.should_not =~ /Christian.*don't.*stat.*play.*game/i
+# Then /see.*[welcome|user].*"(.*)".?(\w+).*stat.*s/ do |name, statistics|
+#   if statistics == "with"
+#     # "Welcome back, Christian"
+#     page.body.should_not =~ /Christian.*don't.*stat.*play.*game/i
 
-    # Christian doesn't have any stats. You need to play a game.
-    page.body.should =~ /.*(welcome|again|back).*(Christian)/i
+#     # Christian doesn't have any stats. You need to play a game.
+#     page.body.should =~ /.*(welcome|again|back).*(Christian)/i
 
-  elsif statistics == "without"
-    # "Welcome back, Christian"
-    page.body.should_not =~ /.*(welcome|again|back).*(Christian)/i
+#   elsif statistics == "without"
+#     # "Welcome back, Christian"
+#     page.body.should_not =~ /.*(welcome|again|back).*(Christian)/i
 
-    # Christian doesn't have any stats. You need to play a game.
-    page.body.should =~ /Christian.*stat.*play.*game/i
-  end
-end
+#     # Christian doesn't have any stats. You need to play a game.
+#     page.body.should =~ /Christian.*stat.*play.*game/i
+#   end
+# end
 
 #-----------------------------------------------------------------------------
 
@@ -58,14 +28,17 @@ When /^I create a new user$/ do
   fill_in 'user[screen_name]', :with => "winnerjohn65"
   fill_in 'user[password]', :with => "password"
   fill_in 'user[password_confirmation]', :with => "password"
-  fill_in 'address[street]', :with => "123 Maple Avenue"
-  fill_in 'address[city]', :with => "Raleigh"
-  fill_in 'address[state]', :with => "NC"
-  fill_in 'address[zip]', :with => "98101"
+  fill_in 'user[address_attributes][street]', :with => "123 Maple Avenue"
+  fill_in 'user[address_attributes][city]', :with => "Raleigh"
+  fill_in 'user[address_attributes][state]', :with => "NC"
+  fill_in 'user[address_attributes][zip]', :with => "98101"
   click_button 'Create User'
 end
 
-When /^I sign in$/ do
+When /sign.in.*(sign\w*.out)|sign.in/ do |sign_out|
+  if sign_out
+    click_on 'Sign out'
+  end
   visit('/users/sign_in')
   fill_in 'user[email]', :with => "user@example.com"
   fill_in 'user[password]', :with => "password"
@@ -78,7 +51,6 @@ Then /^I should be able to start a game$/ do
 end
 
 When /^I sign out$/ do
-  puts page.body
   click_on 'Sign out'
 end
 
